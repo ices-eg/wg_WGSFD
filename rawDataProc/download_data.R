98403cc1c54ee24179d49fc25296cc004b7be3c1
colin millar <colinpmillar@gmail.com>
2017-04-10 14:08:04 +0200

    updated data download script

--- rawDataProc/download_data.R
+++ rawDataProc/download_data.R
@@ -1,21 +1,69 @@
 
 # get packages
 devtools::install_github("hadley/dplyr@v0.4.3")
+# set large java mem
+options(java.parameters='-Xmx8g')
 
 # create directories
 if (!dir.exists("data")) dir.create("data")
 
-# download total abrasion data ----
-
+# connect to DB
 db <- DBI::dbConnect(RSQLServer::SQLServer(), "SQL06", database = 'VMS')
 
-# Fetch results from a fishing hours extraction view
-ICES_LE <- DBI::dbGetQuery(db, "SELECT * FROM [dbo].[_ICES_VMS_Datacall_LE]")
-ICES_VE <- DBI::dbGetQuery(db, "SELECT * FROM [dbo].[_ICES_VMS_Datacall_VE]")
+#submitted_countries <- c('DNK', 'BEL', 'SWE', 'NLD', 'FRA', 'FIN', 'LTU', 'POL', 'Latvia', 'PRT','IRL')
+
+submitted_countries <- c('DNK', 'BEL', 'SWE', 'NLD', 'FIN', 'LTU', 'POL', 'Latvia', 'PRT','IRL')
+
+for (country in submitted_countries) {
+  cat("downloading LE data for ... ", country, "\n")
+  
+  # set up sql command
+  sqlq <- sprintf("SELECT * FROM dbo._2017_ICES_VMS_Datacall_LE WHERE country = '%s'", country)
+  fname <- paste0("data/ICES_LE_", country, ".csv")
+  
+  # fetch
+  res <- DBI::dbSendQuery(db, sqlq)
+  out <- DBI::dbFetch(res, n = 1e5)
+  # save to file
+  write.csv(out, file = fname, row.names = FALSE)
+
+  # Fetch in chunks
+  while (!DBI::dbHasCompleted(res)) {
+    out <- DBI::dbFetch(res, n = 1e5)
+    write.table(out, file = fname, 
+                row.names = FALSE, sep = ",",
+                append = TRUE)
+  }
+  DBI::dbClearResult(res)
+}
+
+
+for (country in submitted_countries) {
+  cat("downloading VMS data for ... ", country, "\n")
+  
+  # connect to databse
+  db <- DBI::dbConnect(RSQLServer::SQLServer(), "SQL06", database = 'VMS')
+
+  # set up sql command
+  sqlq <- sprintf("SELECT * FROM [dbo].[_2017_ICES_VMS_Datacall_VMS] WHERE country = '%s'", country)
+  fname <- paste0("data/ICES_VE_", country, ".csv")
+  
+  # fetch
+  res <- DBI::dbSendQuery(db, sqlq)
+  out <- DBI::dbFetch(res, n = -1)
+  # save to file
+  write.csv(out, file = fname, row.names = FALSE)
+
+  # Fetch in chunks
+  while (!DBI::dbHasCompleted(res)) {
+    out <- DBI::dbFetch(res, n = 1e5)
+    write.table(out, file = fname, 
+                row.names = FALSE, sep = ",",
+                append = TRUE)
+  }
+  DBI::dbClearResult(res)
+
+}
+
 
 DBI::dbDisconnect(db)
-
-# save to file
-write.csv(ICES_LE, file = "data/ICES_LE.csv", row.names = FALSE)
-write.csv(ICES_VE, file = "data/ICES_VE.csv", row.names = FALSE)
-
