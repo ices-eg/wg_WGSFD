4c7100812992bb9bf9734c65df293a74b17e083e
colin millar <colinpmillar@gmail.com>
2017-04-10 14:08:27 +0200

    update of QC R script

--- QC/QC.R
+++ QC/QC.R
@@ -1,39 +1,62 @@
 
-writeDataHandling <- function(le_fname, ve_fname) {
-sprintf(  
-"<!------------------------------------------------------------------------------
-Data handling
----------------------------------------------------------------------------- -->
-```{r, echo=FALSE, results='asis'}
-#Read in latest submission -->
-ICES_LE <- read.csv('%s', stringsAsFactors=FALSE, sep=';', na.strings = 'NULL')
-ICES_VE <- read.csv('%s', stringsAsFactors=FALSE, sep=';', na.strings = 'NULL')
-```
-",
-le_fname, ve_fname)
+# This script creates an Rmd file for a specific country
+makeQCRmd <- function(country, qc) {
+  # make title
+  qc$yaml[grep("title:", qc$yaml)] <- 
+    paste0("title: \"ICES VMS datacall quality check report for ", country, "\"")
+     
+  # fill in file names
+  qc$data <- 
+    sprintf(   
+      paste("<!------------------------------------------------------------------------------",
+            "Data handling",
+            "---------------------------------------------------------------------------- -->",
+            "```{r, echo=FALSE, results='asis'}",
+            "#Read in latest submission -->",
+            "ICES_LE <- read.csv('%s', stringsAsFactors=FALSE, na.strings = 'NULL')",
+            "ICES_VE <- read.csv('%s', stringsAsFactors=FALSE, na.strings = 'NULL')",
+            "```",
+            "", sep = "\n"),
+     paste0("data/ICES_LE_", country, ".csv"), 
+     paste0("data/ICES_VE_", country, ".csv"))
+
+  unlist(qc)
 }
 
 
-# read from template file
-qc <- paste(readLines("QC/QC.Rmd"), collapse = "\n")
-qc1 <- strsplit(qc, "<!-- QCTEMPLATE: header -->")[[1]]
-qc2 <- strsplit(qc1[2], "<!-- QCTEMPLATE: data -->")[[1]]
-qc3 <- strsplit(qc2[2], "<!-- QCTEMPLATE: body -->")[[1]]
-qc <- c(qc1[1], qc2[1], qc3)
+# read and parse from template file
+qc <- readLines("QC/QC.Rmd")
+loc1 <- grep("<!-- QCTEMPLATE: header -->", qc)
+loc2 <- grep("<!-- QCTEMPLATE: data -->", qc)
+loc3 <- grep("<!-- QCTEMPLATE: body -->", qc)
+
+qc <- list(yaml = qc[1:(loc1-1)], 
+           header = qc[loc1:(loc2-1)], 
+           data = qc[loc2:(loc3-1)], 
+           body = qc[loc3:length(qc)])
 
 # update template with country info
 
-country <- "PRT"
+countries <- gsub("ICES_VE_|[.]csv", "", dir("data", pattern = "ICES_VE_*"))
 
-fname <- paste0("QC/QC_", country,".Rmd")
-qc[1] <- 
-  gsub("ICES VMS datacall quality check report", 
-     paste0("ICES VMS datacall quality check report for", country), qc[1])
+for (country in countries) {
+  cat("Running QC for ... ", country, "\n")
+  
+  # set up file names
+  fname <- paste0("QC/QC_", country,".Rmd")
 
-qc[3] <- writeDataHandling("data/ICES_LE_PRT_2016.csv", "data/ICES_VMS_PRT_2016.csv")
+  # fillin and write template
+  cat(makeQCRmd(country, qc), sep = "\n", file = fname)
 
-# write template
-cat(qc, sep = "\n", file = fname)
+  # run template
+  rmarkdown::render(fname, knit_root_dir = "../")
+}
+  
 
-# run template
-rmarkdown::render(fname, knit_root_dir = "../")
+
+
+
+
+
+
+
