f9aa5510c84c8a1bc7069abd800c114bbaf2273d
colin millar <colinpmillar@gmail.com>
2017-05-28 23:19:44 +0200

    update download data script

--- spatialPolygonsProducts/db-getdata.R
+++ spatialPolygonsProducts/db-getdata.R
@@ -12,24 +12,60 @@ if (!dir.exists("data/fishingPressure")) dir.create("data/fishingPressure")
 conn <- odbcDriverConnect(connection = dbConnection)
 
 # get submitted countries
-sqlq <- "select top(10) * from  dbo.tblFinal_Maps_withFormula"
-out <- sqlQuery(conn, sqlq)
+#sqlq <- paste(readLines("rawDataProc/create_total.sql"), collapse = "\n")
+#sqlq <- paste("select top(10) * FROM (", sqlq, ") as x  WHERE year = '2016'")
+#sqlQuery(conn, sqlq)
 
-years <- 2009:2016
+
+years <- 2016
 
 for (year in years) {
-  cat("downloading Fishing pressure data for ... ", year, "\n")
+  cat("downloading Fishing pressure total data for ... ", year, "\n")
   flush.console()
   
   # set up sql command
-  sqlq <- sprintf("select * from  dbo.tblFinal_Maps_withFormula WHERE Year = '%s'", year)
-  fname <- paste0("data/fishingPressure/OSPAR_intensity_data", year, ".csv")
+  sqlq <- paste(readLines("rawDataProc/create_total.sql"), collapse = "\n")
+  sqlq <- sprintf(paste("select * FROM (", sqlq, ") as x  WHERE year = '%s'"), year)
+  fname <- paste0("data/fishingPressure/OSPAR_intensity_data_total_", year, ".csv")
   
   # fetch
   out <- sqlQuery(conn, sqlq)
   # save to file
   write.csv(out, file = fname, row.names = FALSE)
 }
+
+
+for (year in years) {
+  cat("downloading Fishing pressure JNCC grouping data for ... ", year, "\n")
+  flush.console()
+  
+  # set up sql command
+  sqlq <- paste(readLines("rawDataProc/create_JNCC_groups.sql"), collapse = "\n")
+  sqlq <- sprintf(paste("select * FROM (", sqlq, ") as x  WHERE year = '%s'"), year)
+  fname <- paste0("data/fishingPressure/OSPAR_intensity_data_jncc_", year, ".csv")
+
+  # fetch
+  out <- sqlQuery(conn, sqlq)
+  # save to file
+  write.csv(out, file = fname, row.names = FALSE)
+}
+
+
+for (year in years) {
+  cat("downloading Fishing pressure Benthis grouping data for ... ", year, "\n")
+  flush.console()
+  
+  # set up sql command
+  sqlq <- paste(readLines("rawDataProc/create_Benthis_groups.sql"), collapse = "\n")
+  sqlq <- sprintf(paste("select * FROM (", sqlq, ") as x  WHERE year = '%s'"), year)
+  fname <- paste0("data/fishingPressure/OSPAR_intensity_data_benthis_", year, ".csv")
+  
+  # fetch
+  out <- sqlQuery(conn, sqlq)
+  # save to file
+  write.csv(out, file = fname, row.names = FALSE)
+}
+
 
 # disconnect
 odbcClose(conn)
