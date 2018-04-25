a436f10d2e6a633c39f563fa472e89d4a3f5ce92
colin millar <colinpmillar@gmail.com>
2018-04-25 11:17:08 +0200

    move to 2018 data folder

--- QC/download_data.R
+++ QC/download_data.R
@@ -6,24 +6,25 @@ dbConnection <- 'Driver={SQL Server};Server=SQL06;Database=VMS;Trusted_Connectio
 
 # create directories
 if (!dir.exists("data")) dir.create("data")
-if (!dir.exists("data/QC")) dir.create("data/QC")
+if (!dir.exists("data/QC_2018")) dir.create("data/QC_2018")
 
 # connect to DB
 conn <- odbcDriverConnect(connection = dbConnection)
 
 # get submitted countries
-#sqlq <- sprintf("SELECT DISTINCT country FROM dbo._2017_ICES_VMS_Datacall_LE", country)
-#sqlQuery(conn, sqlq)$country
+#sqlQuery(conn, "SELECT DISTINCT country FROM dbo._2017_ICES_VMS_Datacall_LE")$country
+#countries <- c("DNK", "BEL", "IRL", "SWE", "nld", "FRA", "POL", "Latvia", "EST", "PRT", "LTU")
 #countries <- c('DNK', 'BEL', 'SWE', 'NLD', 'FRA', 'FIN', 'LTU', 'POL', 'Latvia', 'PRT','IRL', 'GBR')
 #countries <-  c('FRA', 'LTU', 'Latvia', 'GBR')
-countries <-  c('ICE')
+#countries <-  'IRL'
+countries <-  c('FIN', 'LTU', 'EST', 'IRL')
 
 for (country in countries) {
   cat("downloading LE data for ... ", country, "\n")
   
   # set up sql command
   sqlq <- sprintf("SELECT * FROM dbo._2017_ICES_VMS_Datacall_LE WHERE country = '%s'", country)
-  fname <- paste0("data/QC/ICES_LE_", country, ".csv")
+  fname <- paste0("data/QC_2018/ICES_LE_", country, ".csv")
   
   # fetch
   out <- sqlQuery(conn, sqlq)
@@ -37,7 +38,7 @@ for (country in countries) {
   
   # set up sql command
   sqlq <- sprintf("SELECT * FROM [dbo].[_2017_ICES_VMS_Datacall_VMS] WHERE country = '%s'", country)
-  fname <- paste0("data/QC/ICES_VE_", country, ".csv")
+  fname <- paste0("data/QC_2018/ICES_VE_", country, ".csv")
   
   # fetch
   out <- sqlQuery(conn, sqlq)
