44a851704b213ce990c288a60076c1c6ba60123d
Colin Millar <colinpmillar@gmail.com>
2019-01-11 14:17:58 +0100

    update zipping up script

--- spatialPolygonsProducts/report_zipup.R
+++ spatialPolygonsProducts/report_zipup.R
@@ -1,7 +1,7 @@
 
 # HELCOM
 
-setwd("spatialPolygonsProducts/maps/_ICES_VMS_Datacall_VMS/HELCOM")
+setwd("spatialPolygonsProducts/maps/_ICES_VMS_Datacall_VMS/2017/HELCOM")
 
 zip(zipfile = "../../ICES.2017.Maps-HELCOM-spatial-data-fishing-intensity.zip", 
     files = list.files(".", recursive = TRUE))
@@ -9,7 +9,7 @@ zip(zipfile = "../../ICES.2017.Maps-HELCOM-spatial-data-fishing-intensity.zip",
 setwd("../../../..")
 
 
-setwd("spatialPolygonsProducts/shapefiles/_ICES_VMS_Datacall_VMS/HELCOM/")
+setwd("spatialPolygonsProducts/shapefiles/_ICES_VMS_Datacall_VMS/2017/HELCOM/")
 
 zip(zipfile = "../../ICES.2017.Shapefiles-HELCOM-spatial-data-fishing-intensity.zip", 
     files = list.files(".", recursive = TRUE))
@@ -19,7 +19,7 @@ setwd("../../../..")
 
 # OSPAR - 2017
 
-setwd("spatialPolygonsProducts/maps/_ICES_VMS_Datacall_VMS/OSPAR")
+setwd("spatialPolygonsProducts/maps/_ICES_VMS_Datacall_VMS/2017/OSPAR")
 
 zip(zipfile = "../../ICES.2017.Maps-OSPAR-spatial-data-fishing-intensity.zip", 
     files = list.files(".", recursive = TRUE))
@@ -27,7 +27,7 @@ zip(zipfile = "../../ICES.2017.Maps-OSPAR-spatial-data-fishing-intensity.zip",
 setwd("../../../..")
 
 
-setwd("spatialPolygonsProducts/shapefiles/_ICES_VMS_Datacall_VMS/OSPAR/")
+setwd("spatialPolygonsProducts/shapefiles/_ICES_VMS_Datacall_VMS/2017/OSPAR/")
 
 zip(zipfile = "../../ICES.2017.Shapefiles-OSPAR-spatial-data-fishing-intensity.zip", 
     files = list.files(".", recursive = TRUE))
@@ -37,7 +37,7 @@ setwd("../../../..")
 
 # OSPAR - 2018
 
-setwd("spatialPolygonsProducts/maps/_ICES_VMS_Datacall_VMS/OSPAR")
+setwd("spatialPolygonsProducts/maps/_ICES_VMS_Datacall_VMS/2018/OSPAR")
 
 zip(zipfile = "../../ICES.2018.Maps-OSPAR-spatial-data-fishing-intensity.zip", 
     files = list.files(".", recursive = TRUE))
@@ -45,7 +45,7 @@ zip(zipfile = "../../ICES.2018.Maps-OSPAR-spatial-data-fishing-intensity.zip",
 setwd("../../../..")
 
 
-setwd("spatialPolygonsProducts/shapefiles/_ICES_VMS_Datacall_VMS/OSPAR/")
+setwd("spatialPolygonsProducts/shapefiles/_ICES_VMS_Datacall_VMS/2018/OSPAR/")
 
 zip(zipfile = "../../ICES.2018.Shapefiles-OSPAR-spatial-data-fishing-intensity.zip", 
     files = list.files(".", recursive = TRUE))
