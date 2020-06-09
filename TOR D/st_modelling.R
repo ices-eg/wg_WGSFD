961fa9d398dffeaeaefa6ae48d48b7f781c0980b
mwoillez <mathieu.woillez@ifremer.fr>
2020-06-09 15:37:48 +0200

    Add title
    Update authorship when needed

--- TOR D/st_modelling.R
+++ TOR D/st_modelling.R
@@ -1,3 +1,10 @@
+####################################################################################################
+## TOR D: Identifying potential drivers and describing spatial conflicts of fisheries in the past ##
+##        and future on displacement of fishing activities over various time-scales               ##   
+##                                                                                                ##
+## Author: M.Woillez                                                                              ## 
+## Date: 09.06.2020                                                                               ## 
+####################################################################################################
 
 # Need R version 3.5.1 !!!!
 
