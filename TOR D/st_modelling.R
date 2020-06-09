aaf68defec56f7e03b28887290f263565614b1f6
Mathieu WOILLEZ <mathieu.woillez@ifremer.fr>
2020-06-09 23:37:51 +0200

    Cleaner version of st_modelling.R

--- TOR D/st_modelling.R
+++ TOR D/st_modelling.R
@@ -8,7 +8,7 @@
 
 # Need R version 3.5.1 !!!!
 
-### Load necessary libraries
+### Load libraries
 library(INLA) 
 library(data.table) 
 library(fields) 
@@ -20,26 +20,28 @@ library(graphics)
 library(ggplot2) 
 library(PerformanceAnalytics)
 
-# specification for saving
-patfig<-paste(getwd(),"/RFig/",sep="")
+# create folder to save figures
+dir.create(file.path(getwd(), "RFig"))
+patfig<-paste(getwd(),"/RFig",sep="")
 
 # read data
 df <- fread("data_modelling.csv")
 head(df)
+dim(df)
 
-# create zeroes and rounding
-df$fishing_hours[is.na(df$fishing_hours)] <- 0
-df$fishing_hours <- round(df$fishing_hours)
+# select a metier
+df <- df[df$metier_benth=="OT_DMF",]
+dim(df)
 
-# histogram fishing hours
-tiff(paste(patfig,"/Fig_A_.tiff",sep=""),compression="lzw",width=6,height=6,res=200,units="in")
+# histogram of the explanatory variable: fishing hours
+tiff(paste(patfig,"/Fig_A.tiff",sep=""),compression="lzw",width=6,height=6,res=200,units="in")
 hist(df$fishing_hours,100000,xlim=c(0,20))
 dev.off()
 
-# my color palettes  
+# color palette
 jet.colors <- colorRampPalette(c("#00007F","blue","#007FFF","cyan","#7FFF7F","yellow","#FF7F00","red","#7F0000"))
 
-# blocking
+# Data visualization
 ix <- seq(min(df$lon,na.rm=T)-0.025,max(df$lon,na.rm=T)+0.025,0.05)
 iy <- seq(min(df$lat,na.rm=T)-0.025,max(df$lat,na.rm=T)+0.025,0.05)
 it <- seq(min(df$year,na.rm=T)-0.5,max(df$year,na.rm=T)+0.5,1)
@@ -48,7 +50,7 @@ xcut <- cut(df$lon,ix,include.bounds=T)
 ycut <- cut(df$lat,iy,include.bounds=T)
 mcut <- cut(df$month,im,include.bounds=T)
 tcut <- cut(df$year,it,include.bounds=T)
-Z <- tapply(df$fishing_hours,list(xcut,ycut,mcut,tcut),sum)
+Z <- tapply(df$fishing_hours,list(xcut,ycut,mcut,tcut),sum,na.rm=T)
 year <- sort(unique(df$year))
 for(i in 1:length(unique(df$year))){
   for(j in 1:12){
@@ -59,36 +61,22 @@ for(i in 1:length(unique(df$year))){
   }
 }
 
-# check
-tmp <- tapply(df$fishing_hours,list(xcut,ycut,mcut,tcut),length)
-summary(tmp)
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
-#  1.0     1.0     1.0     1.1     1.0    10.0 1776240
-    
-# # blocking over months and years
-# Z <- tapply(df$fishing_hours,list(xcut,ycut),sum,na.rm=T)
-# tiff(paste(patfig,"/Fig_C_aggregated_over_years_and_months.tiff",sep=""),compression="lzw",width=6,height=6,res=200,units="in")
-# image.plot(ix,iy,Z,main="",col=jet.colors(100),xlab="Longitude (in degree)",ylab="Latitude (in degree)")
-# map("worldHires",col=8,fill=T,add=T);box()   
-# dev.off()
-# 
-# # format dataset
-# tmp1 <- aggregate(fishing_hours ~ lon + lat, data = df, FUN = sum, na.rm=T);dim(tmp1)
-# tmp2 <- aggregate(cbind(in_shore,distance_coast_avg,bpi5,bpi10,bpi30,bpi50,bpi75,tac_ple,tac_sol,
-#                         mud_percent,sand_percent,gravel_percent,total_d50,tidalvelmean,
-#                         oil_price,sea_bottom_temp) ~ lon + lat, data = df, FUN = mean, na.rm=T);dim(tmp2)
-# tmp <- merge(x=tmp1, y=tmp2, all.x=T, by=c("lon","lat"));dim(tmp)
-# tmp <- na.omit(tmp);dim(tmp)
-# head(tmp)
+# size of the dataset if all c_squares were informed 
+length(Z)
 
-# 
-df <- df[,-c("metier_benth","totweight","totvalue","kw_fishinghours")];dim(df)
-df <- na.omit(df);dim(df)
+# actual size of the dataset
+dim(df)[1]
+
+# Zero values are missing, but covariates values are missing too at those zeroes location... Database need to be revised...
+
+# Delete unwanted variables 
+df <- df[,-c("metier_benth","totweight","totvalue","kw_fishinghours")]
+
+# sample size per months and years
 table(df$year,df$month)
 
-# sub-sample
+# sub-sample data
 set.seed(123)
-tmp0 <- df
 tmp <- NULL
 for(y in unique(df$year)){
   for(m in  unique(df$month)){
@@ -109,14 +97,14 @@ tiff(paste(patfig,"/Fig_D_hist_.tiff",sep=""),compression="lzw",width=6,height=6
 hist(tmp$fishing_hours,100000,xlim=c(0,20))
 dev.off()
 
-# plots
+# plot explanatory variable
 tiff(paste(patfig,"/Fig_D.tiff",sep=""),compression="lzw",width=6,height=6,res=200,units="in")
 ggplot() + 
   geom_point(aes(x = tmp$lon, y = tmp$lat, colour = tmp$fishing_hours), size = 2, alpha = 1) +
   scale_colour_gradientn(colours = tim.colors(100))
 dev.off()
 
-# plots covars
+# plots co-variates
 tiff(paste(patfig,"/Fig_D_in_shore.tiff",sep=""),compression="lzw",width=6,height=6,res=200,units="in")
 ggplot() + 
   geom_point(aes(x = tmp$lon, y = tmp$lat, colour = as.numeric(tmp$in_shore)), size = 2, alpha = 1) +
@@ -198,6 +186,7 @@ ggplot() +
   scale_colour_gradientn(colours = tim.colors(100))
 dev.off()
 
+# Other plots
 tiff(paste(patfig,"/Fig_E.tiff",sep=""),compression="lzw",width=6,height=6,res=200,units="in")
 par(mfrow = c(3, 2))
 plot(tmp$lon, tmp$fishing_hours, cex = 0.5, xlab = "Longitude")
@@ -208,28 +197,20 @@ plot(tmp$distance_coast_avg, tmp$fishing_hours, cex = 0.5, xlab = "Average dista
 dev.off()
 par(mfrow = c(1, 1))
 
-m.bad <- inla.mesh.create(cbind(tmp$lon,tmp$lat))
-tiff(paste(patfig,"/Fig_F_mesh1.tiff",sep=""),compression="lzw",width=6,height=6,res=200,units="in")
-plot(m.bad, asp=1, main="")
-points(cbind(tmp$lon,tmp$lat),col="red",pch=19,cex=0.5)
-dev.off()
+# Create shapefile for study area
 
-# Create shapefile for study area - to be used later
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# load North Sea coastlines
 data("worldHiresMapEnv")
 NS_poly <- map("worldHires",  fill=TRUE, col="transparent", plot=FALSE, xlim=c(-5,8.5), ylim=c(51,62))
-NS_poly$names
 IDs <- sapply(strsplit(NS_poly$names, ":"), function(x) x[1])
 NS <- map2SpatialPolygons(NS_poly, IDs=IDs,proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
 
 # Defining boundary of the study area
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 box.sel <- matrix(c(-4.5,9.5,9.5,-4.5,-4.5,50,50,61,61,50), ncol=2)
 
-# Load square polygon function (to be used later)
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# From Bakka et al. (2017)
+# Load square polygon function
 square.polygon = function(xlim, ylim, ret.SP = F, id=runif(1)){
+  # From Bakka et al. (2017)
   # - ret.SP=T : Return a SpatialPolygons object
   # - ret.SP=F : Return a Polygon object
   xlim = range(xlim); ylim = range(ylim)
@@ -246,27 +227,17 @@ square.polygon = function(xlim, ylim, ret.SP = F, id=runif(1)){
 # Splitting polygon into land and water polygon  
 square <- square.polygon(xlim=range(box.sel[ ,1]), ylim=range(box.sel[ ,2]), ret.SP = T)
 square@proj4string <- NS@proj4string
+poly.land <- gIntersection(NS, square) 
+poly.water <- gDifference(square, poly.land) 
 
-poly.land <- gIntersection(NS, square) #land polygon
-poly.water <- gDifference(square, poly.land) # water polygon 
-
-# To have a better overview of what has been done so far
+# View polygons and estimation data
 tiff(paste(patfig,"/Fig_F_polygons.tiff",sep=""),compression="lzw",width=6,height=6,res=200,units="in")
 plot(poly.water, xlim=range(box.sel[ ,1]), ylim=range(box.sel[ ,2]), axes=T,col="lightblue")
 plot(poly.land,col="grey70",add=T)
 points(cbind(tmp$lon,tmp$lat),col="red",pch=19,cex=0.5)
 dev.off()
 
-m1 <- inla.mesh.2d(cbind(tmp$lon,tmp$lat), max.edge = c(0.45, 1), cutoff = 0.2)
-tiff(paste(patfig,"/Fig_F_mesh2.tiff",sep=""),compression="lzw",width=6,height=6,res=200,units="in")
-plot(m1, asp = 1, main = "")
-plot(poly.land, col = 3, add=T)
-points(tmp$lon, tmp$lat, pch = 19, cex = 0.5, col = "red")
-dev.off()
-
-prdomain <- inla.nonconvex.hull(cbind(tmp$lon,tmp$lat), -0.03, -0.05, resolution = c(100, 100))
-
-### create final mesh
+# create triangulation mesh
 prmesh <- inla.mesh.2d(boundary = poly.water, max.edge = c(0.45, 1), cutoff = 0.2)
 tiff(paste(patfig,"/Fig_F_mesh3.tiff",sep=""),compression="lzw",width=6,height=6,res=200,units="in")
 plot(prmesh, asp = 1, main = "")
@@ -274,44 +245,48 @@ plot(poly.land, col = 3, add=T)
 points(tmp$lon, tmp$lat, pch = 19, cex = 0.5, col = "red")
 dev.off()
 
-### define SPDE
+# Construct the SPDE object
 spde <- inla.spde2.matern(prmesh)
 
-### Observation matrix (A) for each year
-table(repl <- tmp$year-2008)#year index from 1:6
+# Observation structure for estimation data
+table(repl <- as.numeric(factor(paste(tmp$year,sprintf("%02d", tmp$month),sep="-")))) # from 1 to 72
 dim(A <- inla.spde.make.A(prmesh, repl=repl, loc=cbind(tmp$lon,tmp$lat)))
 
-### Spatial index 
-mesh.index <- inla.spde.make.index(name='spatial.field', n.spde=spde$n.spde, n.repl=6)
+# Observation structure for field prediction 
+mesh.index <- inla.spde.make.index(name='spatial.field', n.spde=spde$n.spde, n.repl=length(unique(repl)))
 
-### Stack data
-head(tmp,1)
-stk.dat <- inla.stack(data=list(y=tmp$fishing_hours), 
-                   tag='est', A=list(A, 1), 
-                   effects=list(mesh.index, 
-                                data.frame(Intercept=1, tmp[,c(2:length(tmp))])))
+# Stack data
+stk.dat <- inla.stack(data=list(y=tmp$fishing_hours), A=list(A, 1), 
+                   effects=list(c(mesh.index, list(Intercept=1)), list(tmp[,4:(length(tmp)-3)])),
+                   tag='est')
 
-# The INLA stack --------------------------------------------------------------------------------------------------- ICI !!!
+# define model formula
+formula <- (y ~ -1 + 
+              in_shore +
+              distance_coast_avg +
+              bpi5 +
+              bpi10 +
+              bpi30 +
+              bpi50 +
+              bpi75 +
+              tac_ple +
+              tac_sol +
+              mud_percent +
+              sand_percent +
+              gravel_percent +
+              total_d50 +
+              tidalvelmean +
+              oil_price +
+              sea_bottom_temp +
+              f(field, model=spde, group=field.group, control.group=list(model="ar1")))
 
-stk.dat <- inla.stack(data = list(y = Y), A = list(A, 1), tag = "est", 
-                      effects = list(c(mesh.index, list(Intercept = 1)), 
-                                     list(long = inla.group(tmp$lon), lat = inla.group(tmp$lat),
-                                          in_shore = inla.group(in_shore),
-                                          distance_coast_avg = inla.group(distance_coast_avg),
-                                          bpi5 = inla.group(bpi5),
-                                          bpi10 = inla.group(bpi10),
-                                          bpi30 = inla.group(bpi30),
-                                          bpi50 = inla.group(bpi50),
-                                          bpi75 = inla.group(bpi75),
-                                          tac_ple = inla.group(tac_ple),
-                                          tac_sol = inla.group(tac_sol),
-                                          mud_percent = inla.group(mud_percent),
-                                          sand_percent = inla.group(sand_percent),
-                                          gravel_percent = inla.group(gravel_percent),
-                                          total_d50 = inla.group(total_d50),
-                                          tidalvelmean = inla.group(tidalvelmean),
-                                          oil_price = inla.group(oil_price),
-                                          sea_bottom_temp = inla.group(sea_bottom_temp))))
+# fit the spatio-temporal model
+# + perform cross-validation and compute the DIC value
+r.s <- inla(formula, family = "nbinomial", data = inla.stack.data(stk.dat), verbose = TRUE, 
+            control.predictor = list(A = inla.stack.A(stk.dat), compute = TRUE), 
+            control.compute = list(dic = TRUE, cpo = TRUE, waic = T))
 
-# save results
-save.image("Input.RData")
+# check criterion
+r.s$waic$waic
+mean(log(r.s$cpo$cpo))
+
