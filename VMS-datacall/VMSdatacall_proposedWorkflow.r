dbf067687f77926a2f14286788429bda4c465e38
Jeppe Olsen <jepol@aqua.dtu.dk>
2021-03-24 09:31:22 +0100

    Update VMSdatacall_proposedWorkflow.r
    Edited yearsToSubmit from 2009:2019 to 2009:2020

--- VMS-datacall/VMSdatacall_proposedWorkflow.r
+++ VMS-datacall/VMSdatacall_proposedWorkflow.r
@@ -44,7 +44,7 @@ intvThres     <- 240  #Maximum difference in time interval in minutes to prevent
 lanThres      <- 1.5  #Maximum difference in log10-transformed sorted weights
 
 #- Re-run all years as we have new field for no. vessels
-yearsToSubmit <- sort(2009:2019)
+yearsToSubmit <- sort(2009:2020)
 
 #- Set the gear names for which automatic fishing activity is wanted
 #  It is important to fill out the gears you want to apply auto detection for
