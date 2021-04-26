b44d5f5822cd99297bb0e9d7700bd4f2bd7ea46c
Roi Martinez <roimarful@gmail.com>
2021-04-27 00:10:09 +0100

    Update VMSdatacall_proposedWorkflow.r

--- VMS-datacall/VMSdatacall_proposedWorkflow.r
+++ VMS-datacall/VMSdatacall_proposedWorkflow.r
@@ -894,11 +894,12 @@ for(year in yearsToSubmit){
   tacsatEflalo$INTV      <- tacsatEflalo$INTV / 60
   tacsatEflalo$LENGTHCAT <- cut(tacsatEflalo$VE_LEN, breaks=c(0, 8, 10, 12, 15, 200))
   tacsatEflalo$LENGTHCAT <- as.character(tacsatEflalo$LENGTHCAT)
-  tacsatEflalo$LENGTHCAT[which(tacsatEflalo$LENGTHCAT == "(0,8]")] <- "<8"
-  tacsatEflalo$LENGTHCAT[which(tacsatEflalo$LENGTHCAT == "(8,10]")] <- "8-10"
-  tacsatEflalo$LENGTHCAT[which(tacsatEflalo$LENGTHCAT == "(10,12]")] <- "10-12"
-  tacsatEflalo$LENGTHCAT[which(tacsatEflalo$LENGTHCAT == "(12,15]")] <- "12-15"
-  tacsatEflalo$LENGTHCAT[which(tacsatEflalo$LENGTHCAT == "(15,200]")] <- ">15"
+  ### following new ICES DATSU vocabulary format for vessel length categories http://vocab.ices.dk/?ref=1502
+  tacsatEflalo$LENGTHCAT[which(tacsatEflalo$LENGTHCAT == "(0,8]")] <- "A"
+  tacsatEflalo$LENGTHCAT[which(tacsatEflalo$LENGTHCAT == "(8,10]")] <- "B"
+  tacsatEflalo$LENGTHCAT[which(tacsatEflalo$LENGTHCAT == "(10,12]")] <- "C"
+  tacsatEflalo$LENGTHCAT[which(tacsatEflalo$LENGTHCAT == "(12,15]")] <- "D"
+  tacsatEflalo$LENGTHCAT[which(tacsatEflalo$LENGTHCAT == "(15,200]")] <- "E"
 
   RecordType <- "VE"
 
@@ -946,15 +947,19 @@ for(year in yearsToSubmit){
   eflalo <- merge(eflalo, res, by = c("VE_COU", "VE_REF", "LE_CDAT"))
   eflalo$INTV <- eflalo$INTV / eflalo$nrRecords
   eflalo$kwDays <- eflalo$VE_KW * eflalo$INTV
-  eflalo$tripInTacsat <- ifelse(eflalo$FT_REF %in% tacsatp$FT_REF, "Yes", "No")
+  
+ ### following new ICES DATSU vocabulary format for vessel length categories http://vocab.ices.dk/?ref=316
+  eflalo$tripInTacsat <- ifelse(eflalo$FT_REF %in% tacsatp$FT_REF, "Y", "N")
 
   eflalo$LENGTHCAT <- cut(eflalo$VE_LEN, breaks = c(0, 8, 10, 12, 15, 200))
   eflalo$LENGTHCAT <- ac(eflalo$LENGTHCAT)
-  eflalo$LENGTHCAT[which(eflalo$LENGTHCAT == "(0,8]")] <- "<8"
-  eflalo$LENGTHCAT[which(eflalo$LENGTHCAT == "(8,10]")] <- "8-10"
-  eflalo$LENGTHCAT[which(eflalo$LENGTHCAT == "(10,12]")] <- "10-12"
-  eflalo$LENGTHCAT[which(eflalo$LENGTHCAT == "(12,15]")] <- "12-15"
-  eflalo$LENGTHCAT[which(eflalo$LENGTHCAT == "(15,200]")] <- ">15"
+  
+  ### following new ICES DATSU vocabulary format for vessel length categories http://vocab.ices.dk/?ref=1502
+  eflalo$LENGTHCAT[which(eflalo$LENGTHCAT == "(0,8]")] <- "A"
+  eflalo$LENGTHCAT[which(eflalo$LENGTHCAT == "(8,10]")] <- "B"
+  eflalo$LENGTHCAT[which(eflalo$LENGTHCAT == "(10,12]")] <- "C"
+  eflalo$LENGTHCAT[which(eflalo$LENGTHCAT == "(12,15]")] <- "D"
+  eflalo$LENGTHCAT[which(eflalo$LENGTHCAT == "(15,200]")] <- "E"
 
   RecordType <- "LE"
 
@@ -1061,6 +1066,6 @@ colnames(table2Save) <-
   )
 
 ## Save the final table 1 and table 2 . Headers and quotes have been removed to be compatible with required submission format.  
-write.table(table1Save, file.path(outPath, "table1Save.csv"),row.names=FALSE,col.names=FALSE,sep=",",quote=FALSE)
-write.table(table2Save, file.path(outPath, "table2Save.csv"),row.names=FALSE,col.names=FALSE,sep=",",quote=FALSE)
+write.table(table1Save, na = "", file.path(outPath, "table1Save.csv"),row.names=FALSE,col.names=FALSE,sep=",",quote=FALSE)
+write.table(table2Save, na = "", file.path(outPath, "table2Save.csv"),row.names=FALSE,col.names=FALSE,sep=",",quote=FALSE)
 
