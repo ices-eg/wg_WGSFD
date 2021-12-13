b55bc12269d98b3eefe2e998b8cd980817d49066
Colin Millar <colin.millar@ices.dk>
2021-12-13 15:24:04 +0100

    add functions which will become icesDATSU

---
+++ VMS-datacall/hackathon_workflow/colin/DATSU_utilities.R
@@ -0,0 +1,22 @@
+
+# functions to be written here:
+
+# * submit file
+
+# * get list of feild names for DATSU format
+
+# * get if feild is mandatory
+
+# * check if feild is mandatory
+
+# * Check if feild has vocabulary
+
+# * check vector against a vocabulary
+
+# * check a data.frame against a DATSU format
+#    - check feild names
+#    - check mandatory feilds for NULL etc.
+#   - check vocabularied feilds against vocab
+#   - check order of feilds
+#   - check for missing feilds
+#   - check for extra feilds
