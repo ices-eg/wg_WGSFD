e4b40fa9794d3104b097635f982379ec3df6bf64
Colin Millar <colin.millar@ices.dk>
2021-12-13 15:47:13 +0100

    * add function to get dataset version IDs and add dependencies

--- VMS-datacall/hackathon_workflow/colin/DATSU_utilities.R
+++ VMS-datacall/hackathon_workflow/colin/DATSU_utilities.R
@@ -1,15 +1,41 @@
 
+if (!require("icesVocab")) {
+  install.packages("icesVocab", repos = "https://ices-tools-prod.r-universe.dev")
+  library(icesVocab)
+}
+
+require(jsonlite)
+require(httr)
+
 # functions to be written here:
 
-# * submit file
+datsu_api <- function(service) {
+  paste0("https://datsu.ices.dk/API/", service)
+}
 
-# * get list of feild names for DATSU format
+# * get list of formats
 
-# * get if feild is mandatory
+#' @examples
+#' formats <- getDataverIDs()
+#' vms_format <- formats[grep("vms", tolower(formats$description)),]
+#' vms_format
+getDataverIDs <- function() {
+  url <- datsu_api("getDataverIDs")
+  res <- httr::GET(url)
+
+  content(res, simplifyVector = TRUE)
+}
+
+
+# * get list of field names for DATSU format
+
+# * get if field is mandatory
+
+# * get if field has vocabulary
+
+# * get vocabulary for field
 
 # * check if feild is mandatory
-
-# * Check if feild has vocabulary
 
 # * check vector against a vocabulary
 
@@ -20,3 +46,5 @@
 #   - check order of feilds
 #   - check for missing feilds
 #   - check for extra feilds
+
+# * submit file
