51f787371d5f20e0023afb4a198f4465eef13299
Colin Millar <colin.millar@ices.dk>
2021-12-14 14:41:55 +0100

    add feild types and code to create dummy data

--- VMS-datacall/hackathon_workflow/colin/DATSU_worksheet.R
+++ VMS-datacall/hackathon_workflow/colin/DATSU_worksheet.R
@@ -1,6 +1,9 @@
 
 source("VMS-datacall/hackathon_workflow/colin/DATSU_utilities.R")
 
+datasetverID <- 145
+RecordType <- "VE"
+
 formats <- getDataverIDs()
 vms_format <- formats[grep("vms", tolower(formats$description)),]
 vms_format
@@ -12,9 +15,40 @@ getDataFieldsDescription(145, "VE")
 
 datsuFieldNames(145, "VE")
 
+datsuFieldTypes(145, "VE")
+
 datsuIsMandatory(145, "VE", "AverageVesselLength")
 
 datsuHasVocabulary(145, "VE", "AverageVesselLength")
 datsuHasVocabulary(145, "VE", "CountryCode")
 
 datsuGetVocabulary(145, "VE", "CountryCode")
+
+# check a table
+n <- 10
+good <- as.list(makeEmptyTable(datasetverID, RecordType))
+for (i in which(datsuFieldTypes(145, "VE") == "integer")) {
+  good[[i]] <- sample(1:10, n, replace = TRUE)
+}
+for (i in which(datsuFieldTypes(145, "VE") == "numeric")) {
+  good[[i]] <- runif(n)
+}
+for (i in which(datsuFieldTypes(145, "VE") == "character")) {
+  good[[i]] <- rep("non-vocabulary", n)
+}
+
+for (i in which(datsuHasVocabulary(145, "VE"))) {
+  good[[i]] <-
+    sample(
+      datsuGetVocabulary(145, "VE", datsuFieldNames(145, "VE")[i]),
+      n,
+      replace = TRUE
+    )
+}
+
+good$MetierL6 <- paste0(good$MetierL4, "_", good$MetierL5, "_", good$LowerMeshSize, "_", good$UpperMeshSize, "_0")
+good$RecordType <- "VE"
+good$Year <- "2020"
+
+good <- as.data.frame(good)
+good
